31858f728f85c3ae858af86253baede540a88394
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-29 21:02:26 +0900

    feat:백엔드 폴더

---
+++ vue-til-server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parser: "babel-eslint",
+  // extends: ["prettier", "plugin:prettier/recommended"],
+  // rules: {
+  //   "prettier/prettier": ["error", { endOfLine: "auto" }],
+  // },
+};
