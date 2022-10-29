4a34783b4e2f12e97054d09b44b7524e8c160cca
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-29 22:44:26 +0900

    feat: [홈 - router] 초기 페이지 설정

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -5,6 +5,10 @@ Vue.use(VueRouter);
 export default new VueRouter({
   routes: [
     {
+      path: '/',
+      redirect: '/login',
+    },
+    {
       path: '/login',
       //코드 스플리팅 -> 페이지가많을 경우에 해당페이지만 가져오기
       component: () => import('@/views/LoginPage.vue'),
