2e446ef2ad51a3f9ada90ccc4beb19648ef0607f
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-30 17:16:39 +0900

    feat:마크업, input 반복 처리

---
+++ vue-til/src/components/SignupForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form @submit="submitFrom">
+      <input-item
+        v-for="item in inputList"
+        :key="item.id"
+        :inputType="item.inputType"
+        :inputId="item.id"
+        :inputText="item.inputText"
+        :v-model="item.vModel"
+      />
+    </form>
+    <button type="submit">login</button>
+  </div>
+</template>
+
+<script>
+import InputItem from './common/InputItem.vue';
+export default {
+  data() {
+    return {
+      inputList: [
+        {
+          id: 'username',
+          inputType: 'text',
+          inputText: 'id:',
+          vModel: this.username,
+        },
+        {
+          id: 'password',
+          inputType: 'text',
+          inputText: 'pw:',
+          vModel: this.password,
+        },
+        {
+          id: 'nickmame',
+          inputType: 'text',
+          inputText: 'nickmame:',
+          vModel: this.nickmame,
+        },
+      ],
+      username: '',
+      password: '',
+      nickmame: '',
+    };
+  },
+  components: { InputItem },
+  methods: {
+    submitFrom() {},
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-til/src/components/common/InputItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input :type="inputType" :id="inputId" />
+    <label :for="inputId">{{ inputText }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['inputType', 'inputId', 'inputText'],
+};
+</script>
+
+<style></style>
