10993952ac0a04c68d4274a03bd53e3ef303018b
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-31 20:49:18 +0900

    feat: [get - data] 학습노트 data 조회

--- vue-til/src/api/index.js
+++ vue-til/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { setInterceptors } from './common/interceptors';
 
+//axios 초기화 함수
 function creacteInstance() {
   const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
@@ -10,12 +11,19 @@ function creacteInstance() {
 
 const instance = creacteInstance();
 
+//회원가입 api
 function registerUser(userData) {
   return instance.post('signup', userData);
 }
 
+//로그인 api
 function loginUser(userData) {
   return instance.post('login', userData);
 }
 
-export { registerUser, loginUser };
+//학습 노트 데이터를 조회하는 api
+function fetchPosts() {
+  return instance.get('post');
+}
+
+export { registerUser, loginUser, fetchPosts };
