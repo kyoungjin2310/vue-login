71d85b09847884adf23d4cd1373815d5a0737238
kyoungjin2310 <kyoungjin2310@gmail.com>
2022-10-31 21:15:44 +0900

    feat:[loading] loading 화면 구현

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -2,23 +2,13 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
+      <loading-spinner v-if="isLoading" />
+      <ul v-else>
         <post-list-item
           v-for="postItem in postItems"
           :key="postItem._id"
           :item="postItem"
         ></post-list-item>
-        <!-- <li v-for="postItem in postItems" :key="postItem._id">
-          <div class="post-title">
-            {{ postItem.title }}
-          </div>
-          <div class="post-contents">
-            {{ postItem.contents }}
-          </div>
-          <div class="post-title">
-            {{ postItem.createdAt }}
-          </div>
-        </li> -->
       </ul>
     </div>
   </div>
@@ -27,17 +17,21 @@
 <script>
 import { fetchPosts } from '@/api/index';
 import PostListItem from '../components/posts/PostListItem.vue';
+import LoadingSpinner from '../components/common/LoadingSpinner.vue';
 export default {
-  components: { PostListItem },
+  components: { PostListItem, LoadingSpinner },
   data() {
     return {
       postItems: [],
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
       try {
+        this.isLoading = true;
         const { data } = await fetchPosts();
+        this.isLoading = false;
         this.postItems = data.posts;
         console.log(data.posts);
       } catch (error) {
